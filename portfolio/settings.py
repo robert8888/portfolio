f4208f189c09aba997bda388527231bc816f9729
unknown <robert.kami88@gmail.com>
2021-01-28 02:15:20 +0100

    change static configuration for heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'static_compiled'),
 ]
 
