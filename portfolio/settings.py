563233b5fb40131d9fb54bcdb9e84212685e8c0d
unknown <robert.kami88@gmail.com>
2021-03-20 18:04:45 +0100

    fix missing static reference in cke editor skin

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -313,4 +313,4 @@ ADMIN_REORDER = (
 )
 
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles = False, secret_key = False, databases = False, test_runner = False)
