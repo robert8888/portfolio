40fea573b69e9c3f68b4b056ca41f8e7ab6b82f4
unknown <robert.kami88@gmail.com>
2021-05-08 23:54:08 +0200

    Set assets compresion onlinse set storgae to s3 - cdn

--- portfolio/storage.py
+++ portfolio/storage.py
@@ -12,3 +12,16 @@ class AssetStorage(S3Boto3Storage):
     file_overwrite = True
     default_acl = 'private'
     custom_domain = os.getenv('AWS_S3_CUSTOM_DOMAIN')
+
+    """
+    S3 storage backend that saves the files locally, too.
+    """
+    def __init__(self, *args, **kwargs):
+        super(CachedS3Boto3Storage, self).__init__(*args, **kwargs)
+        self.local_storage = get_storage_class(
+            "compressor.storage.CompressorFileStorage")()
+
+    def save(self, name, content):
+        self.local_storage._save(name, content)
+        super(CachedS3Boto3Storage, self).save(name, self.local_storage._open(name))
+        return name
