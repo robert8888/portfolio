4c6b28737aa1d176f4e06c364206b1fa5efbe231
unknown <robert.kami88@gmail.com>
2021-06-02 00:42:45 +0200

    fixing heorku setting for dommain - cfrs cookie domain

--- portfolio/settings/base_settings.py
+++ portfolio/settings/base_settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 ALLOWED_HOSTS = ["127.0.0.1", 'rkaminski.herokuapp.com','rkam.dev']
 
-CSRF_TRUSTED_ORIGINS = ["127.0.0.1", 'rkaminski.herokuapp.com','rkam.dev']
+# CSRF_TRUSTED_ORIGINS = ["127.0.0.1", 'rkaminski.herokuapp.com','rkam.dev']
+CSRF_COOKIE_DOMAIN = ".rkam.dev"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
