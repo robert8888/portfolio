7f3578d69fb295dcaaf53c8ce89f9619b09e9e87
unknown <robert.kami88@gmail.com>
2021-06-02 00:24:47 +0200

    fixing heorku setting for dommai - add trusterd cfrs origins

--- portfolio/settings/base_settings.py
+++ portfolio/settings/base_settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 ALLOWED_HOSTS = ["127.0.0.1", 'rkaminski.herokuapp.com','rkam.dev']
 
+CSRF_TRUSTED_ORIGINS = ["127.0.0.1", 'rkaminski.herokuapp.com','rkam.dev']
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
