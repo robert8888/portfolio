68275c30281ddaa879a644e8de5973d57602a36f
unknown <robert.kami88@gmail.com>
2021-06-02 23:46:37 +0200

    Fixing quatro cv style

--- portfolio/settings/base_settings.py
+++ portfolio/settings/base_settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 ALLOWED_HOSTS = ["127.0.0.1", 'rkaminski.herokuapp.com','rkam.dev']
 
-CSRF_TRUSTED_ORIGINS = ["127.0.0.1:8000", '.rkaminski.herokuapp.com','.rkam.dev', 'rkam.dev']
+CSRF_TRUSTED_ORIGINS = ["127.0.0.1:8000", 'localhost','.rkaminski.herokuapp.com','.rkam.dev', 'rkam.dev']
 CSRF_COOKIE_DOMAIN =  ["localhost", '.rkaminski.herokuapp.com','.rkam.dev', 'rkam.dev']
 
 
