8ae1ebf8ec999345eae7ea1cd1114f9377cb2f75
unknown <robert.kami88@gmail.com>
2021-06-03 01:39:52 +0200

    Adding setting allowing remove domain from crawl indexin -fix robots.txt view -remove debugin statement

--- portfolio/robots_view.py
+++ portfolio/robots_view.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import render
 
 def process_request(request):
-   print(request.get_host(), settings.INDEXED_DOMAINS, request.get_host() in settings.INDEXED_DOMAINS)
    if request.get_host() in settings.INDEXED_DOMAINS:
        return render(request, 'robots.txt', content_type='text/plain')
    else:
