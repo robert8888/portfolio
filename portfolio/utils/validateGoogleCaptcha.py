222c529d37bd42d65acc7bd78073e0f739f5d6dc
unknown <robert.kami88@gmail.com>
2021-06-02 17:35:35 +0200

    Clera print statements and final csfr token config

--- portfolio/utils/validateGoogleCaptcha.py
+++ portfolio/utils/validateGoogleCaptcha.py
@@ -6,14 +6,11 @@ gcaptcha_key = os.getenv("GCAPTCHA_SECRET_KEY")
 gcaptcha_url = os.getenv('GCAPTCHA_VALIDATION_URL')
 
 def validateCaptcha(token):
-    print(token)
     captchaData = {
         "secret" :  gcaptcha_key,
         "response" : token
     }
 
     res = requests.post(gcaptcha_url, data = captchaData)
-
-    print(res.json())
 
     return res.status_code == 200 and res.json()['success']
