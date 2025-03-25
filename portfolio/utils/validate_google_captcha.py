d89356fe2162fc7a2527d1ac83a6c1064585b5a4
robert_kam <robert.kaminski@websolutionsnyc.com>
2025-03-25 19:52:27 +0100

    update captcha validation

--- portfolio/utils/validate_google_captcha.py
+++ portfolio/utils/validate_google_captcha.py
@@ -1,20 +1,44 @@
+# import requests
+# import json
+# import os
+#
+# gcaptcha_key = os.getenv("GCAPTCHA_SECRET_KEY")
+# gcaptcha_url = os.getenv('GCAPTCHA_VALIDATION_URL')
+#
+# def validate_captcha(token):
+#     print("DEBUG-validate captcha",  token, gcaptcha_key)
+#     captchaData = {
+#         "secret" :  gcaptcha_key,
+#         "response" : token
+#     }
+#     headers = {"Content-Type": "application/x-www-form-urlencoded"}
+#
+#     res = requests.post(gcaptcha_url, data = captchaData, headers = headers);
+#
+#     print("DEBUG-validate captcha", res.json())
+#
+#     return res.status_code == 200 and res.json()['success']
+
 import requests
-import json
+import requests
 import os
 
 gcaptcha_key = os.getenv("GCAPTCHA_SECRET_KEY")
-gcaptcha_url = os.getenv('GCAPTCHA_VALIDATION_URL')
+gcaptcha_url = os.getenv("GCAPTCHA_VALIDATION_URL")
 
 def validate_captcha(token):
-    print("DEBUG-validate captcha",  token, gcaptcha_key)
-    captchaData = {
-        "secret" :  gcaptcha_key,
-        "response" : token
+    print("DEBUG-validate captcha", token, gcaptcha_key)
+
+    params = {
+        "secret": gcaptcha_key,
+        "response": token
     }
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
-    res = requests.post(gcaptcha_url, data = captchaData, headers = headers);
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    res = requests.post(gcaptcha_url, params=params, headers=headers)
 
-    print("DEBUG-validate captcha", res.json())
+    print("DEBUG-validate captcha response:", res.json())
 
-    return res.status_code == 200 and res.json()['success']
+    return res.status_code == 200 and res.json().get("success", False)
